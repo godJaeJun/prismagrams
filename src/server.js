16cacf1bb9df5a7db5056dacead6b1614dd55165
asapz <asapz@naver.com>
2019-07-24 11:23:57 +0900

    Setting Up the Server like the Pros

--- src/server.js
+++ src/server.js
@@ -1,18 +1,17 @@
 require("dotenv").config();
 import {GraphQLServer} from "graphql-yoga";
+import logger from "morgan";
+import schema from "./schema";
 
 const PORT = process.env.PORT || 4000;      //.env 의 PORT를 가져온다 만약 없다면 4000번으로 설정
-const typeDefs = `
-    type Query{
-        hello: String!
-    }
-`;
-// graphql 서버 생성 type정의와 resolver들이 필요함
-const resolvers = {
-    Query:{
-        hello : () => "Hi"
-    }
-}
-const server = new GraphQLServer({typeDefs,resolvers});
 
-server.start({port : PORT},()=>console.log(`Server running on port http://localhost:${PORT}`));
+const server = new GraphQLServer({ schema });
+
+// server.express 라고 하면 express 서버에 접근할 수 있다.
+// 미들웨어 추가
+server.express.use(logger("dev"));
+
+server.start({ port: PORT }, () =>
+    console.log(`✅ Server running on http://localhost:${PORT}`)
+);
+
