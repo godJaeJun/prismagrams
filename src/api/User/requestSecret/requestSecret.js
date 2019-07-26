1b0667b5ba8c24508db415b31039fb81dc7dac56
asapz <asapz@naver.com>
2019-07-26 10:14:23 +0900

    toggleLike and addComment Resolver

--- src/api/User/requestSecret/requestSecret.js
+++ src/api/User/requestSecret/requestSecret.js
@@ -2,12 +2,11 @@ import {generateSecret, sendSecretMail} from "../../../utils";
 import { prisma } from "../../../../generated/prisma-client";
 export default {
     Mutation:{
-        requestSecret: async (_, args, { request }) => {
+        requestSecret: async (_, args) => {
             const { email } = args;
             const loginSecret = generateSecret();
             console.log(loginSecret)
             try{
-                throw Error();
                 await sendSecretMail(email, loginSecret);
                 await prisma.updateUser({data:{loginSecret}, where:{email}})
                 return true
